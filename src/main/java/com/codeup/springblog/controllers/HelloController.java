ae25076d261604680c024267b9434f18fa3bf42c
Chris Cyr <chris.dana.cyr@gmail.com>
2020-03-25 12:12:20 -0500

    added several changes

--- src/main/java/com/codeup/springblog/controllers/HelloController.java
+++ src/main/java/com/codeup/springblog/controllers/HelloController.java
@@ -1,7 +1,11 @@
 package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 class HelloController {
@@ -9,18 +13,55 @@ class HelloController {
     @GetMapping("/hello")
     @ResponseBody
     public String hello() {
+        System.out.println("Inside hello()");
         return "Hello from Spring!";
     }
 
     @GetMapping("/hello/{name}")
-    @ResponseBody
-    public String sayHello(@PathVariable String name) {
-        return "Hello " + name + "!";
+    public String sayHello(@PathVariable String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
     }
+
+    @GetMapping("/join")
+    public String showJoinFor(){
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String joinCohort(@RequestParam(name = "cohort") String cohort, Model model) {
+        model.addAttribute("cohort","Welcome to " + cohort + "!");
+        return "join";
+    }
+//    @GetMapping(path = "/hello/{name}")
+//    @ResponseBody
+//    public String sayHello(@PathVariable String name) {
+//        System.out.println("Inside sayHello()");
+//        return "Hello " + name + "!";
+//    }
 
     @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
     @ResponseBody
     public String addOne(@PathVariable int number) {
         return number + " plus one is " + (number + 1) + "!";
+    }
+
+    @GetMapping("/fruit")
+    public String fruit(Model model){
+        List<String> fruitBasket= new ArrayList<>();
+        fruitBasket.add("apple");
+        fruitBasket.add("kiwi");
+        fruitBasket.add("pear");
+        fruitBasket.add("mango");
+        fruitBasket.add("watermelon");
+        fruitBasket.add("grapes");
+        fruitBasket.add("starfruit");
+        fruitBasket.add("papaya");
+        fruitBasket.add("japanese pear");
+        fruitBasket.add("pineapple");
+        fruitBasket.add("lemon");
+        fruitBasket.add("durian");
+        model.addAttribute("fruitBasket", fruitBasket);
+        return "fruit";
     }
 }
--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,35 +1,73 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.Post;
+import com.codeup.springblog.repositories.PostRepo;
+import org.hibernate.service.spi.InjectService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.model.IModel;
+
+import java.util.ArrayList;
 
 @Controller
-class PostController {
+public class PostController {
+
+    private PostRepo postDao;
+
+    public PostController(PostRepo postDao) {
+        this.postDao = postDao;
+    }
 
     @GetMapping("/posts")
-    @ResponseBody
-    public String index() {
-        return "This is a landing page for the blog site";
+    public String getPosts(Model model){
+        model.addAttribute("posts", postDao.findAll());
+        return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String id(@PathVariable int id) {
-        return "This page shows the page for post id # " + id;
+    public String getPost(@PathVariable int id, Model model){
+        Post post1 = new Post(id, "Europa's First Post", "Remote Learning Today!");
+        model.addAttribute("title", post1.getTitle());
+        model.addAttribute("body", post1.getBody());
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")
     @ResponseBody
-    public String create() {
-        return "This is a page for viewing the form to create a post";
+    public String getCreatePostForm(){
+        return "view the form for creating a post";
     }
 
     @PostMapping("/posts/create")
     @ResponseBody
-    public String createPost() {
-        return "This posts a blog post";
+    public String createPost(){
+        return "create a new post";
     }
+
+    @PostMapping("/posts/{id}/delete")
+    public String delete(@PathVariable long id){
+        // delete post
+        postDao.deleteById(id);
+        return "redirect:/posts";
+    }
+
+    @GetMapping("/posts/{id}/edit")
+    public String editForm(@PathVariable long id, Model model) {
+        Post postToEdit = postDao.getOne(id);
+        model.addAttribute("post", postToEdit);
+        return "posts/edit";
+    }
+
+    @PostMapping("/posts/{id}/edit")
+    public String updatePost(@PathVariable long id, @RequestParam String title, @RequestParam String body) {
+        Post p = postDao.getOne(id);
+        p.setTitle(title);
+        p.setBody(body);
+        postDao.save(p);
+        return "redirect:/posts";
+    }
+
+
 }
